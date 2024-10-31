8a4d06f0813ba12ccc1235be86678f41d556d6e5
unknown <danielpanf@gmail.com>
2024-10-31 18:07:52 +0100

    added models

---
+++ exercise.wwwapi/Model/FoodItem.cs
@@ -0,0 +1,6 @@
+﻿namespace exercise.wwwapi.Model
+{
+    public class FoodItem
+    {
+    }
+}
---
+++ exercise.wwwapi/Model/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.wwwapi.Model
+{
+    [Table("Order")]
+    public class Order
+    {
+        [Key]
+        [Column("OrderID")]
+        public int OrderID { get; set; }
+
+        [Column("Created At")]
+        public string CreatedAt { get; set; }
+
+        [Column("CustomerID")]
+    }
+}
